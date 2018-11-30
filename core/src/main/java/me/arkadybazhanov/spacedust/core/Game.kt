17a49027037b5ab50720ba85f2ea1164222dbcb4
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2018-11-30 03:32:37 +0300

    Revert infix function example

--- core/src/main/java/me/arkadybazhanov/spacedust/core/Game.kt
+++ core/src/main/java/me/arkadybazhanov/spacedust/core/Game.kt
@@ -1,18 +1,6 @@
 package me.arkadybazhanov.spacedust.core
 
-import kotlin.random.*
-
 class Game {
-
-    private val rand = Random(0)
-
-    infix fun Int.d(a: Int) = (1..this).sumBy { 1 + rand.nextInt(a) }
-
-    fun main() {
-        val damage = 2 d 12
-
-    }
-
     fun update() {
         // TODO
     }
