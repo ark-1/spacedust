c750b9be1376ec15d0893a1a902b4ba9fcd71ee4
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2019-02-18 16:04:29 +0300

    Add zoom experiments

--- app/src/main/java/me/arkadybazhanov/spacedust/MainActivity.kt
+++ app/src/main/java/me/arkadybazhanov/spacedust/MainActivity.kt
@@ -1,10 +1,13 @@
 package me.arkadybazhanov.spacedust
 
 import android.app.*
+import android.hardware.Camera
 import android.os.*
 import android.view.WindowManager.LayoutParams.*
 
 class MainActivity : Activity() {
+
+    private lateinit var view: GameView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +15,17 @@ class MainActivity : Activity() {
         window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
 
         setContentView(R.layout.activity_main)
+        view = findViewById(R.id.game_view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        view.camera = Camera.open()
+    }
+    private var inPreview = false
+    override fun onPause() {
+        super.onPause()
+        if (inPreview) view.camera.stopPreview()
+        view.camera.release()
     }
 }
