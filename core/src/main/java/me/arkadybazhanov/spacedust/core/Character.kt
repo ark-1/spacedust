f7890fed6394260aeacf0ce3e62f12ece2f9b96d
Alexey Trilis <aatrilis2000@gmail.com>
2019-02-19 18:17:44 +0300

    Change movement system

--- core/src/main/java/me/arkadybazhanov/spacedust/core/Character.kt
+++ core/src/main/java/me/arkadybazhanov/spacedust/core/Character.kt
@@ -4,6 +4,9 @@ interface Character : Comparable<Character> {
     val id: Int
     var level: Level
     var position: Position
+    val directions: List<Direction>
+
+    fun canMoveTo(position: Position): Boolean
 
     suspend fun getCharacterMove(): PerformableEvent
 
